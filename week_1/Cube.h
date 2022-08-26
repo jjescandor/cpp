1adf3517f6652048b04d6c25172ebc6216b5b49f
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-26 11:26:55 -0400

    initial commit

---
+++ week_1/Cube.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Cube{
+    public:
+        double getVolume();
+        double getSuraceArea();
+        void setLength(double length);
+
+    private:
+    double length_;
+}
